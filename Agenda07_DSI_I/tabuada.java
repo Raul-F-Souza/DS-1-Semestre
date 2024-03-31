17cc956f13e5a91321a54d960ac92865aaf3ab2a
Raul F. De Souza <115673322+Ael1an@users.noreply.github.com>
2024-03-31 16:59:34 -0300

    Atividade da agenda 07

---
+++ Agenda07_DSI_I/tabuada.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class tabuada {
+    
+    public static void main(String[] args) {
+        
+        int escolha = Integer.parseInt(JOptionPane.showInputDialog("Digite qual tabuada deseja visualizar (6, 7, 8, 9, 10). \nDigite apenas números.")); // Solicitando a escolha de tabuada
+
+        for (int i = 1; i <= 10; i++) { // Inicio do For limitado a 10 repetições
+            int resultado = escolha * i; // Calculo antecipado do resultado
+            System.out.println(escolha + " X " + i + " = " + resultado); // Impressão da tabuada
+        } // Fim do For
+    }
+}
