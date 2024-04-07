9a721895aeb1c2613bc4420b91502008b22e75d6
Raul F. De Souza <115673322+Ael1an@users.noreply.github.com>
2024-04-07 09:30:47 -0300

    Criação do programa para atividade facultativa da agenda 08

---
+++ Agenda08_DSI_I/tabuadaWhile.java
@@ -0,0 +1,20 @@
+package Agenda08_DSI_I;
+
+import javax.swing.JOptionPane;
+
+public class tabuadaWhile {
+
+    public static void main(String[] args) {
+
+        int escolha, resultado, aux = 1; // Declaração de variáveis utilizadas no programa
+
+        escolha = Integer.parseInt(JOptionPane.showInputDialog("Digite o número para mostrar sua tabuada até 10:")); // Solicitando o número para tabuada
+
+        do { // ìnicio do Do While
+            resultado = aux * escolha; // Realizando o calculo do resultado antecipadamente e resgistrando na variável 'resultado'
+            System.out.println(escolha + " X " + aux + " = " + resultado); // Exibindo tabuada do número solicitado
+
+            aux++; // Adicionado +1 na variável auxiliar que também opera como contador
+        } while (aux <= 10); // Fim do Do While após 10 repetições
+    } // Fim do programa
+} // Fim da Classe
