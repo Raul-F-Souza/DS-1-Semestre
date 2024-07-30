21ac9ce6cb84c0c5ab6e25e28b17ce1fbdaba75c
Raul Souza <raulsouza1654@gmail.com>
2024-07-30 11:19:51 -0300

    Ajustes na organização

--- atividadeExtra.java
+++ atividadeExtra.java
@@ -1,14 +1,18 @@
 import javax.swing.JOptionPane;
 
 public class atividadeExtra {
-    
-    public static void main(String[] args) {
-        
-        int escolha = Integer.parseInt(JOptionPane.showInputDialog("Digite qual tabuada deseja visualizar (6, 7, 8, 9, 10). \nDigite apenas números.")); // Solicitando a escolha de tabuada
 
-        for (int i = 1; i <= 10; i++) { // Inicio do For limitado a 10 repetições
-            int resultado = escolha * i; // Calculo antecipado do resultado
-            System.out.println(escolha + " X " + i + " = " + resultado); // Impressão da tabuada
-        } // Fim do For
-    }
-}
+    public static void main(String[] args) {
+
+        int escolha, resultado, aux = 1; // Declaração de variáveis utilizadas no programa
+
+        escolha = Integer.parseInt(JOptionPane.showInputDialog("Digite o número para mostrar sua tabuada até 10:")); // Solicitando o número para tabuada
+
+        do { // ìnicio do Do While
+            resultado = aux * escolha; // Realizando o calculo do resultado antecipadamente e resgistrando na variável 'resultado'
+            System.out.println(escolha + " X " + aux + " = " + resultado); // Exibindo tabuada do número solicitado
+
+            aux++; // Adicionado +1 na variável auxiliar que também opera como contador
+        } while (aux <= 10); // Fim do Do While após 10 repetições
+    } // Fim do programa
+} // Fim da Classe
